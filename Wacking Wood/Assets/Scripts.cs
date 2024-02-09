e7e48c92798dcfbe3c416930842fb0fa081e6023
Pogromel <Binkowski-areek@o2.pl>
2024-02-09 13:20:03 +0000

    Not Finished Main Menu

---
+++ Wacking Wood/Assets/Scripts.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Scripts : MonoBehaviour
+{
+    [Header("Load levels")] 
+    public string _NewGameLevel;
+
+
+
+}
