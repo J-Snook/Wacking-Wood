4be9759981e24feba95fe1529a78015b97deb2a3
Pogromel <Binkowski-areek@o2.pl>
2024-02-11 03:18:29 +0000

    More Updates

--- Wacking Wood/Assets/Scripts/MainMenu_Controller.cs
+++ Wacking Wood/Assets/Scripts/MainMenu_Controller.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
+
 public class MainMenu_Controller : MonoBehaviour
 {
     [Header("Volume Settings")] 
     [SerializeField] private TMP_Text volumeValue = null;
     [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
     [SerializeField] private GameObject confirmPrompt = null;
+    
+    [Header("Gameplay Settings")] 
+    [SerializeField] private TMP_Text ControllerSenTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 4;
+    public int mainControllerSen = 4;
     
     [Header("Load levels")] 
     public string _newGameLevel;
@@ -50,10 +61,43 @@ public class MainMenu_Controller : MonoBehaviour
     public void VolumeApply()
     {
         PlayerPrefs.SetFloat("Volume", AudioListener.volume);
-        StartCoroutine(VolumeConfirmation());
+        StartCoroutine(Confirmation());
     }
 
-    public IEnumerator VolumeConfirmation()
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        ControllerSenTextValue.text = sensitivity.ToString("0");
+        
+    }
+
+    public void GameApply()
+    {
+        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+        StartCoroutine(Confirmation());
+    }
+    
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+        if (MenuType == "Game")
+        {
+            ControllerSenTextValue.text = defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            GameApply();
+            
+        }
+    }
+
+    public IEnumerator Confirmation()
     {
         confirmPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
